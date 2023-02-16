6ee77c57ac59be69422e3dbd3511b4c32ab9a55b
Marcelo Cruz <marcelo.cruzf11@gmail.com>
2023-02-16 00:28:51 -0300

    style: animation, menu

--- src/components/menu/styled.js
+++ src/components/menu/styled.js
@@ -18,13 +18,40 @@ export const MenuContainer = styled.div`
   width: 100%;
   height: 100%;
   z-index: 20;
-  background-color: black;
+  background-image: linear-gradient(
+    10deg,
+    black,
+    #050505,
+    #0c0c0c,
+    #050505,
+    black
+  );
 
   display: flex;
   align-items: center;
   justify-content: center;
 
+  transition: all 0.7s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  animation: menuNav 0.5s ease-out;
+
+  @keyframes menuNav {
+    0% {
+      opacity: 0;
+      transform: translateX(12rem);
+    }
+
+    80% {
+      transform: translateX(-3rem);
+    }
+
+    100% {
+      opacity: 1;
+      transform: translate(0);
+    }
+  }
+
   li {
+    font-size: 2rem;
     margin: 5rem;
     cursor: pointer;
   }
--- src/projects/projects.js
+++ src/projects/projects.js
@@ -2,6 +2,7 @@ export const projects = [
   {
     id: 0,
     name: 'Rom Hacking',
+    type: 'other',
     descript:
       'O projeto de rom hacking é uma edição de um jogo, originalmente desenvolvido pela SEGA o jogo chamado Kid Chameleon é um jogo labirinto e diversos mapas... Um desenvolvedor do github fez um "disassembly" no arquivo binário do jogo com isso ele disponibilizou a destruturação em linguagem assembly para quem quiser realizar edições no jogo, junto com ferramentas desenvolvidas por próprios programadores para edição de "mapas, blocos, backgroud do jogo" e um editor hexadecimal para alterar valores em endereços específicos eu realizei o desenvolvimento da minha própria hack rom, intitulada como "Kid Chameleon The Last Showdown" tem seus próprios mapas, paletta de cores e atalhos, o projeto foi realizado por min, porém, com a ajuda de toda uma comunidade por trás do discord oficial do kid chameleon.  Contudo, esse projeto não envolveu uma lógica de programação avançada, pois como as principais funcionalidades do editor para mapas do kid chameleon abstraia grande parte da complexidade do assembly, fazendo que me precupasse apenas com o design dos mapas e da construção da rom hack, com isso, foi meu primeiro trabalho e contato com design, que me fez ter vontade de me tornar um desenvolvedor web.',
     project:
