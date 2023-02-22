c5148678e66e0e75bf114a5ccc377d3f4b78e083
Marcelo Cruz <marcelo.cruzf11@gmail.com>
2023-02-22 19:12:21 -0300

    style: retirando background

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,6 @@ function App() {
       <BrowserRouter>
         <GlobalStyled />
         <BackgroundTheme>
-          <Background />
           <Container>
             <Menu />
             <Routes>
